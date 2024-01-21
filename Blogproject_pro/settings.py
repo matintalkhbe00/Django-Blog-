b8be43406d3e5bc5bf0726c47647c10c075bdc32
KMmatin <matintalkhbe66@gmail.com>
2024-01-21 22:19:19 +0330

    model of post

--- Blogproject_pro/settings.py
+++ Blogproject_pro/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # project app
     'home_app',
     'account_app',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
