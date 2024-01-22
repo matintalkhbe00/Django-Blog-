b5209b5afc752b161784e45e307fb3bf50a4fc3f
KMmatin <matintalkhbe66@gmail.com>
2024-01-22 21:50:05 +0330

    pillow library and blog model

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class post(models.Model):
     title = models.CharField(max_length=70)
     body = models.TextField()
-    img = models.ImageField(upload_to='post/' , null=False )
+    # img = models.ImageField(upload_to='post/' , null=False )
     create = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
