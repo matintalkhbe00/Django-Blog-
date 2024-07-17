20943fe0c84c89bc0fa121aede617eb4ac4bb9e4
kmmatin66 <65290307+kmmatin66@users.noreply.github.com>
2024-07-18 01:34:38 +0330

    Django project Blog site

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    # path('post_details/<slug:slug>', views.post_detail, name='post_data'),
-    path('post_details/<slug:slug>', views.PostDetailView.as_view(), name='post_data'),
+    path('post_details/<slug:slug>', views.post_detail, name='post_data'),
+    # path('post_details/<slug:slug>', views.PostDetailView.as_view(), name='post_data'),
     path('list/', views.posts_list, name='post_list'),
     path('categories/<int:pk>', views.categories_post, name='posts_categories'),
     path('search/', views.search_post, name='posts_search'),
